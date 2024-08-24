05d512c4a8e3c694123b9c37df5bf89ed3131a7e
Phoebe <jolinhappy1990@hotmail.com>
2024-08-25 00:52:16 +0800

    feat: add get data api

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,37 @@
+import { Maze } from '@types'
+import { useEffect, useState } from 'react'
+import MazeMap from '@/components/MazeMap'
+
 export default function App() {
-  return <div>Hello</div>
+  const [mazeData, setMazeData] = useState<Maze[]>([])
+
+  useEffect(() => {
+    const getMazeData = () => {
+      fetch('http://localhost:8080/api/maze')
+        .then(response => {
+          if (response.ok) {
+            return response.json()
+          } else {
+            throw new Error('Network response was not ok.')
+          }
+        })
+        .then(data => {
+          setMazeData(data)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
+    getMazeData()
+  }, [])
+  return (
+    <div className="flex flex-col items-center gap-6 m-10">
+      {mazeData.map((mazeMap, index) => (
+        <div key={index} className="flex flex-col items-center">
+          <h3 className="text-2xl">地圖 {index + 1}</h3>
+          <MazeMap mazeData={mazeMap} />
+        </div>
+      ))}
+    </div>
+  )
 }
